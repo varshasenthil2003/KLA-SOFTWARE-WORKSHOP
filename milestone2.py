97a40cf71ed80d1934c78c52c156d1f4d0870cfe
varshasenthil2003 <varshasenthil111@gmail.com>
2024-01-12 15:18:37 +0530

    M2

---
+++ milestone2.py
@@ -0,0 +1,51 @@
+import math
+import numpy as np
+import ast 
+
+with open("C:/Users/91948/Desktop/Kla hackathon/Milestone2/Input/Testcase1.txt", "r") as file1:
+        read_content = file1.readlines()
+        print(read_content)
+        
+data={}
+
+for line in read_content:
+        key, value = line.strip().split(':')
+        data[key] = (value)
+
+print(data)
+
+data['WaferDiameter']=int(data['WaferDiameter'])
+
+print(data)
+
+diameter=data['WaferDiameter']
+height, width = data['DieSize'].strip().split('x')
+print(height)
+print(width)
+
+dsv=ast.literal_eval(data['DieShiftVector'])
+print(dsv)
+dsv=list(dsv)
+
+rd=ast.literal_eval(data['ReferenceDie'])
+print(rd)
+rd=list(rd)
+
+print(dsv)
+print(rd)
+
+
+def coord(diameter,height, width, dsv, rd):
+    x=dsv[0]
+    y=dsv[1]
+    print(x,y)
+    llcx=rd[0]
+    llcy=rd[1]
+    print(llcx, llcy)
+    x_, y_=0,0
+    
+    
+
+    
+result=[]
+res=coord(diameter,height, width, dsv, rd )
